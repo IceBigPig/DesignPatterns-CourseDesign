6b8dd45f19be27d03cc3285ded6dc8805cdb5ee6
Mr-code-404 <2398107953@qq.com>
2022-06-17 00:12:17 +0800

    Last Version

--- src/Application.java
+++ src/Application.java
@@ -12,10 +12,7 @@ public class Application {
      * 程序主入口
      */
     public static void main(String[] args) {
-
         CounterUI counterUI = new CounterUI();
         counterUI.createFrame();
-
-        CounterUI counter = (CounterUI) counterUI.clone();
     }
 }
